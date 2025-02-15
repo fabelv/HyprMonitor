5b7f1f5dfae483c91ec76ec67960e3c14ec3da13
Fabio Elvedi <fabio.elvedi@elvfab.net>
2025-02-15 15:19:26 +0100

    clean up repo

--- flake.nix
+++ flake.nix
@@ -1,26 +1,23 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils"; 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   inputs.miniCompileCommands = {
-    url = github:danielbarter/mini_compile_commands/v0.6;
+    url = "github:danielbarter/mini_compile_commands/v0.6";
     flake = false;
   };
-  inputs.koturNixPkgs = {
-    url = github:nkoturovic/kotur-nixpkgs/v0.7.0;
-    flake = false;
-  };
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      package = import ./default.nix {inherit system pkgs;};
+
+  outputs = { self, nixpkgs, flake-utils, miniCompileCommands, ... }:
+    flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = import nixpkgs { inherit system; };
+      package = import ./default.nix {
+        inherit system pkgs;
+      };
     in {
       packages.default = package;
       devShells.default = package.shell;
       formatter = pkgs.alejandra;
     });
 }
+
